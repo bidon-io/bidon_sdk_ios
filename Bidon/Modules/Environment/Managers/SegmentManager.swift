13959ca3a595ed2a9a67cd1a879ca5b9bcff4e64
SK <sk@bidon.org>
2023-06-16 16:25:48 +0200

    Remove default segment data

--- Bidon/Modules/Environment/Managers/SegmentManager.swift
+++ Bidon/Modules/Environment/Managers/SegmentManager.swift
@@ -12,23 +12,60 @@ final class SegmentManager: Segment, Environment {
     @UserDefaultOptional(Constants.UserDefaultsKey.segmentId)
     var id: String?
     
-    var gender: Gender = .other
+    var gender: Gender {
+        get { _gender ?? .other }
+        set { _gender = newValue }
+    }
     
-    var age: Int = .unknown
+    var age: Int {
+        get { _age ?? .unknown }
+        set { _age = newValue }
+    }
     
-    var level: Int = .unknown
+    var level: Int {
+        get { _level ?? .unknown }
+        set { _level = newValue }
+    }
     
-    var isPaid: Bool = false
+    var isPaid: Bool {
+        get { _isPaid ?? false }
+        set { _isPaid = newValue }
+    }
     
-    var inAppAmount: Double = .unknown
+    var inAppAmount: Double {
+        get { _inAppAmount ?? .zero }
+        set { _inAppAmount = newValue }
+    }
+
+    var customAttributes: [String : AnyHashable] {
+        get { _customAttributes ?? [:] }
+    }
     
-    private(set) var customAttributes: [String : AnyHashable] = [:]
+    @Atomic
+    var _gender: Gender?
+    
+    @Atomic
+    var _age: Int?
+
+    @Atomic
+    var _level: Int?
+    
+    @Atomic
+    var _isPaid: Bool?
+    
+    @Atomic
+    var _inAppAmount: Double?
+    
+    @Atomic
+    var _customAttributes: [String : AnyHashable]?
     
     func setCustomAttribute(
         _ customAttribute: AnyHashable,
         for key: String
     ) {
-        customAttributes[key] = customAttribute
+        var attributes = _customAttributes ?? [:]
+        attributes[key] = customAttribute
+        _customAttributes = attributes
     }
 }
 
--- Bidon/Networking/Models/Environment/SegmentModel.swift
+++ Bidon/Networking/Models/Environment/SegmentModel.swift
@@ -15,44 +15,60 @@ struct SegmentResponseModel: Decodable, SegmentResponse {
 
 struct SegmentModel: Encodable {
     var id: String?
-    var gender: String
-    var age: Int
-    var gameLevel: Int
-    var totalInAppsAmount: Double
-    var isPaying: Bool
-    var customAttributes: [String: Any]
+    var gender: String?
+    var age: Int?
+    var gameLevel: Int?
+    var totalInAppsAmount: Double?
+    var isPaying: Bool?
+    var customAttributes: [String: Any]?
+    
+    var ext: [String: Any] {
+        var ext: [String: Any] = [:]
+        if let age = age {
+            ext["age"] = age
+        }
+        if let gender = gender {
+            ext["gender"] = gender
+        }
+        if let isPaying = isPaying {
+            ext["is_paying"] = isPaying
+        }
+        if let totalInAppsAmount = totalInAppsAmount {
+            ext["total_in_apps_amount"] = totalInAppsAmount
+        }
+        if let gameLevel = gameLevel {
+            ext["game_level"] = gameLevel
+        }
+        if let customAttributes = customAttributes {
+            ext["custom_attributes"] = customAttributes
+        }
+        return ext
+    }
     
     enum CodingKeys: CodingKey {
         case id
         case ext
     }
     
-    init(_ segment: Segment) {
+    init(_ segment: SegmentManager) {
         self.id = segment.id
-        self.gender = segment.gender.description.uppercased()
-        self.age = segment.age
-        self.gameLevel = segment.level
-        self.totalInAppsAmount = segment.inAppAmount
-        self.isPaying = segment.isPaid
-        self.customAttributes = segment.customAttributes
+        self.gender = segment._gender?.description.uppercased()
+        self.age = segment._age
+        self.gameLevel = segment._level
+        self.totalInAppsAmount = segment._inAppAmount
+        self.isPaying = segment._isPaid
+        self.customAttributes = segment._customAttributes
     }
     
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encodeIfPresent(id, forKey: .id)
         
-        let ext: [String: Any] = [
-            "age": age,
-            "gender": gender,
-            "is_paying": isPaying,
-            "total_in_apps_amount": totalInAppsAmount,
-            "game_level": gameLevel,
-            "custom_attributes": customAttributes
-        ]
-        
-        let extData = try JSONSerialization.data(withJSONObject: ext)
-        let extString = String(data: extData, encoding: .utf8)
-        try container.encode(extString, forKey: .ext)
+        if !ext.isEmpty {
+            let extData = try JSONSerialization.data(withJSONObject: ext)
+            let extString = String(data: extData, encoding: .utf8)
+            try container.encode(extString, forKey: .ext)
+        }
     }
 }
 
